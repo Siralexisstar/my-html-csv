e484818ab3dec4682a6a2b2e81d8ba2759879ca7
SiralexisStar <alexgarciavillar96@gmail.com>
2026-03-19 21:09:46 +0100

    more info in contact.jsx

--- cv-portfolio/src/components/sections/Contact.jsx
+++ cv-portfolio/src/components/sections/Contact.jsx
@@ -11,12 +11,12 @@ export const Contact = () => {
           I'm currently looking for new opportunities. Whether you have a question or just want to say hi, I'll try my best to get back to you!
         </p>
         <div className="contact__actions">
-          <Button as="a" href="mailto:hello@example.com" size="lg">
+          <Button as="a" href="mailto:[alexgarciavillar96@gmail.com]" size="lg">
             Say Hello
           </Button>
           <div className="contact__socials">
             <a href="https://linkedin.com/in/alejandro-garcía-villar-175875205/" target="_blank" rel="noopener noreferrer" className="contact__link">LinkedIn</a>
-            <a href="https://github.com/alexg" target="_blank" rel="noopener noreferrer" className="contact__link">GitHub</a>
+            <a href="https://github.com/Siralexisstar" target="_blank" rel="noopener noreferrer" className="contact__link">GitHub</a>
           </div>
         </div>
       </div>
